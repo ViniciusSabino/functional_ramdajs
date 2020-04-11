e31a9b29442b6c9b447fa709bc793b1088163586
Vinicius Rocha <vnc.chelsea@gmail.com>
2020-04-11 09:57:45 -0300

    Update functions.js

--- higher-order-functions/functions.js
+++ higher-order-functions/functions.js
@@ -2,9 +2,7 @@
 const getSalaries = (employess) => employess.map((employe) => employe.salary);
 
 // Filtro: salários que estão abaixo de 6 digitos
-const isBelowSixFigures = (salary) => {
-  return `${salary}`.length < 6
-}
+const isBelowSixFigures = (salary) => `${salary}`.length < 6;
 
 // Calcular quanto eles ganham a cada mês
 const calculateMonthlyPaycheck = (median) => median / 12;
